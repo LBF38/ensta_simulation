90fde7fa2eab8be8d9466fcfae2a07860cf23715
LBF38 <contact.lbf38@gmail.com>
2024-01-31 09:32:00 +0100

    refactor(helloworld): update app
    update the app to fix bugs from engine refactoring

--- td/src/helloworld/Etudiant.java
+++ td/src/helloworld/Etudiant.java
@@ -1,13 +1,17 @@
 package helloworld;
 
 import engine.EntiteSimulee;
-import engine.InitData;
 import engine.SimuEngine;
-import enstabretagne.base.time.LogicalDateTime;
+import enstabretagne.base.time.LogicalDuration;
 
 public class Etudiant extends EntiteSimulee {
     public Etudiant(SimuEngine engine, EtudiantInitData initData) {
         super(engine, initData);
-        this.addEvent(new Bonjour(LogicalDateTime.Now(), this, null));
+//        this.send(new Bonjour(LogicalDateTime.Now(), this, null), this);
+    }
+
+    @Override
+    public void init() {
+        send(new Bonjour(this.now().add(LogicalDuration.ofMinutes(15)), this, null));
     }
 }
