ee006757d21ec0bcfc8469628bd6f115af954b64
LBF38 <contact.lbf38@gmail.com>
2024-01-31 12:11:53 +0100

    chore(project): start TD2_1

---
+++ td/src/td2_1/CheckRandom.java
@@ -0,0 +1,19 @@
+package td2_1;
+
+import enstabretagne.base.logger.CategoriesGenerator;
+import enstabretagne.base.logger.Logger;
+import enstabretagne.base.math.MoreRandom;
+
+public class CheckRandom {
+    public static void main(String[] args) {
+        Logger.load();
+        MoreRandom random = new MoreRandom();
+        random.setSeed(123456789);
+        CategoriesGenerator categories = new CategoriesGenerator(-10, 10, 10, 0, 1000);
+        for (int i = 0; i < 1000; i++) {
+            Logger.Data(random.nextGaussian());
+            // TODO: make it work !
+        }
+        Logger.Terminate();
+    }
+}
