ccff11c9e29b56eb83da7b0eaf307d245f187485
LBF38 <contact.lbf38@gmail.com>
2024-02-10 19:15:48 +0100

    refactor(BE): :truck: move WorkshopTime class
    move it to its own file.
    will see in the future if it is useful.

--- BE/src/tatooine/Workshop/InitWorkshop.java
+++ BE/src/tatooine/Workshop/InitWorkshop.java
@@ -3,8 +3,6 @@ package tatooine.Workshop;
 import engine.InitData;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
-import jakarta.json.bind.annotation.JsonbProperty;
-import utils.DateTimeFrenchFormat;
 
 public class InitWorkshop extends InitData {
     private static final Jsonb jsonb = JsonbBuilder.create();
@@ -87,6 +85,9 @@ public class InitWorkshop extends InitData {
         System.out.println("Test if json correct");
         InitWorkshop initWorkshop1 = fromJSON(json);
         testInitWorkshopFromJson(initWorkshop, initWorkshop1);
+        System.out.println(jsonb.toJson(initWorkshop1));
+        // FIXME: make the InitWorkshop class serializable
+        // => the objective is to be able to create an InitWorkshop object from a json string
 
 //        System.out.println("Test if json incorrect");
 //        String json2 = "{\"error\":\"Workshop1\",\"capacity\":10,\"frequenting\":\"FREE\"}";
@@ -116,65 +117,5 @@ public class InitWorkshop extends InitData {
 
     public enum QueueType {
         ORGANIZED, RANDOM
-    }
-
-    public record WorkshopTime(int day, int month, int year, int hour,
-                               int minute,
-                               int second, int nanosecond) {
-        public WorkshopTime() {
-            this(1, 1, 2024, 0, 0, 0, 0);
-        }
-
-        public static WorkshopTime fromJson(String json) {
-            return jsonb.fromJson(json, WorkshopTime.class);
-        }
-
-        public String toJson() {
-            return jsonb.toJson(this);
-        }
-
-        @Override
-        public String toString() {
-            return "WorkshopTime{" + "day=" + day + ", month=" + month + ", year=" + year + ", hour=" + hour + ", minute=" + minute + ", second=" + second + ", nanosecond=" + nanosecond + '}';
-        }
-
-        public String toDateTimeFrenchFormat() {
-            return DateTimeFrenchFormat.of(day, month, year, hour, minute, second, nanosecond);
-        }
-
-        @JsonbProperty("day")
-        public int getDay() {
-            return day;
-        }
-
-        @JsonbProperty("month")
-        public int getMonth() {
-            return month;
-        }
-
-        @JsonbProperty("year")
-        public int getYear() {
-            return year;
-        }
-
-        @JsonbProperty("hour")
-        public int getHour() {
-            return hour;
-        }
-
-        @JsonbProperty("minute")
-        public int getMinute() {
-            return minute;
-        }
-
-        @JsonbProperty("second")
-        public int getSecond() {
-            return second;
-        }
-
-        @JsonbProperty("nanosecond")
-        public int getNanosecond() {
-            return nanosecond;
-        }
     }
 }
