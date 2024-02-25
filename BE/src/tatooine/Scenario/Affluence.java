719b290407659c8cfa3e392e2f4eba7d452782ac
Arthurpers <96655283+Arthurpers@users.noreply.github.com>
2024-02-25 19:37:32 +0100

    Year Scenario (#11)
    * Year Scenario
    
    * chore(project): :art: reformat/lint code
    
    * fix(BE): :ambulance: log error resolved
    
    remove client's logging from RecordData to prevent log crash
    => Excel logs rows limit reached.
    
    ---------
    
    Co-authored-by: LBF38 <contact.lbf38@gmail.com>

---
+++ BE/src/tatooine/Scenario/Affluence.java
@@ -0,0 +1,39 @@
+package tatooine.Scenario;
+
+import enstabretagne.base.time.LogicalDateTime;
+import enstabretagne.base.time.LogicalDuration;
+import utils.DateTimeFrenchFormat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Affluence {
+    private static final List<Double> mondaysArrivals = new ArrayList<>();
+
+    public static List<Double> generateMondaysArrivals(int maxCapacity, Map<Integer, Double> monthlyAttendance) {
+        LogicalDateTime currentDate = new LogicalDateTime(DateTimeFrenchFormat.of(1, 3, 2024));
+        while (currentDate.getDayOfWeek().getValue() != 1) { //Find first Monday of the month
+            currentDate = currentDate.add(LogicalDuration.ofDay(1));
+        }
+        Double currentAffluence = (double) 0;
+        int week = 0;
+        while (currentDate.getMonthValue() < 10) {
+            if (week == 1) {
+                currentAffluence = mondaysArrivals.get(0);
+            } else if (week > 1) {
+                currentAffluence = mondaysArrivals.get(week - 1) + mondaysArrivals.get(week - 2);
+            }
+
+            double monday_arrivals = maxCapacity * monthlyAttendance.get(currentDate.getMonthValue()) - currentAffluence;
+            if (monday_arrivals < 0) {
+                monday_arrivals = 0.0;
+            }
+            mondaysArrivals.add(monday_arrivals);
+            currentDate = currentDate.add(LogicalDuration.ofDay(7));
+            week++;
+        }
+        return mondaysArrivals;
+    }
+}
+
